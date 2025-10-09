75853b78389f4c043e5ad8e025e72434ed3dded6
joe <josepholaniyi820@gmail.com>
2025-10-09 03:53:26 +0100

    Authentication complete

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const SignIn = () => {
   return (
-    <section className=" flex-center sixe-full max-sm:px-6">
+    <section className="  flex-center size-full max-sm:px-6">
       <AuthForm type="sign-in" />
     </section>
   );
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const SignUp = async () => {
   return (
-    <section className=" flex-center sixe-full max-sm:px-6">
+    <section className=" flex-center size-full max-sm:px-6">
       <AuthForm type="sign-up" />
     </section>
   );
